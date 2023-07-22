c21460e31a8cd639e3a7671e480b8f123fca3204
ICY PASCUAL - DEV <129865971+WEBDEVIcy@users.noreply.github.com>
2023-07-22 16:49:30 +0800

    Update index.php

--- sample quiz/index.php
+++ sample quiz/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Quiz</title>
-    <!-- Add Bootstrap CSS -->
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -57,7 +57,7 @@
         </div>
     </div>
 
-    <!-- Add Bootstrap JS and jQuery -->
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
